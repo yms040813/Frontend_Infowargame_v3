04cdfdcca2e3fe192a4c5e7d4551f5d88462bf8b
yun min seop <201411yms@dsm.hs.kr>
2021-09-23 17:07:00 +0900

    new version

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { HashRouter as Router } from 'react-router-dom';
+import Routes from './components/Routes';
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: ${props => props.theme.maxWidth};
+  width: 100%;
+`;
+
+function App() {
+  const [isLoggedIn, useIsLoggedIn] = useState(false);
+
+  return (
+    <Router>
+      <>
+        {isLoggedIn && null} { /* 헤더 들어갈 자리 */ }
+        <Wrapper>
+          <Routes isLoggedIn={isLoggedIn} />
+          { /* Footer 들어갈 자리 */}
+        </Wrapper>
+      </>
+    </Router>
+  )
+}
+
+export default App;

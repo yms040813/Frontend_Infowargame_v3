9b954fb696497754a6fb33e89bb687f3f067b815
MJ <tlcjs40@gmail.com>
2021-11-08 20:27:12 +0900

    <MODIFY> style.js align-content

--- src/components/default/MenuBar.js
+++ src/components/default/MenuBar.js
@@ -4,31 +4,31 @@ import * as S from './style';
 
 const DefaultTemplete = () => {
   return (
-      <S.MenuBarStyled>
-        <MenuBarHeader />
+    <S.MenuBarStyled>
+      <MenuBarHeader />
 
-        <S.NavigatorAreaStyled>
+      <S.NavigatorAreaStyled>
 
-          <S.TopicAreaStyled>
-            
-            <S.LinkStyled to='/ctf'><div className="menuBar--menu">CHALENGE</div></S.LinkStyled>
-            <S.SubAreaStyled>
-              <div>Pwnable</div>
-              <div>Reversing</div>
-              <div>Web</div>
-            </S.SubAreaStyled>
-
-          </S.TopicAreaStyled>
-
-          <S.LinkStyled to='/notice'><div>NOTICE</div></S.LinkStyled>
-          <S.LinkStyled to='/notification'><div>EVENT</div></S.LinkStyled>
-          <S.LinkStyled to='/rank'><div>RANK</div></S.LinkStyled>
-          <S.LinkStyled to='/profile'><div>MYPAGE</div></S.LinkStyled>
-          <S.LinkStyled to='/developers'><div>DEVELOPER</div></S.LinkStyled>
+        <S.TopicAreaStyled>
           
-        </S.NavigatorAreaStyled>
+          <S.LinkStyled to='/ctf'><div className="menuBar--menu">CHALENGE</div></S.LinkStyled>
+          <S.SubAreaStyled>
+            <div>Pwnable</div>
+            <div>Reversing</div>
+            <div>Web</div>
+          </S.SubAreaStyled>
 
-      </S.MenuBarStyled>
+        </S.TopicAreaStyled>
+
+        <S.LinkStyled to='/notice'><div>NOTICE</div></S.LinkStyled>
+        <S.LinkStyled to='/notification'><div>EVENT</div></S.LinkStyled>
+        <S.LinkStyled to='/rank'><div>RANK</div></S.LinkStyled>
+        <S.LinkStyled to='/profile'><div>MYPAGE</div></S.LinkStyled>
+        <S.LinkStyled to='/developers'><div>DEVELOPER</div></S.LinkStyled>
+        
+      </S.NavigatorAreaStyled>
+
+    </S.MenuBarStyled>
   );
 };
 

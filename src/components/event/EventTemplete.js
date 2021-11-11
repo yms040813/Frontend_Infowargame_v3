a03cb6ef9b837377247d7bd1a2661e568ec9a86f
yun min seop <201411yms@dsm.hs.kr>
2021-11-11 19:23:29 +0900

    eventpage connect

---
+++ src/components/event/EventTemplete.js
@@ -0,0 +1,14 @@
+import React from "react";
+import * as S from './style';
+import MenuBar from '../default/MenuBar';
+
+
+const EventTemplete = () => {
+	return(
+		<div className="inner-style">
+      <MenuBar/>
+    </div>
+	);
+      };
+      
+export default EventTemplete;

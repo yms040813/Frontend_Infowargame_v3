b626f9b0ce7a73a69c1fea3b703a78b7fc554869
MJ <tlcjs40@gmail.com>
2021-11-11 16:53:37 +0900

    <ADD> ctfPloblemPage.js params.id

--- src/components/ctfPloblem/ctfTemplete.js
+++ src/components/ctfPloblem/ctfTemplete.js
@@ -3,19 +3,22 @@ import MenuBar from "../default/MenuBar";
 import ProblemBox from "./ProblemBox";
 import * as S from './style'
 import useCtfProblems from "../../hooks/useCtfProblems";
+import { Link } from 'react-router-dom'
 
-const CtfTemplete = () => {
+const CtfTemplete = (paramsId) => {
   const problems = useCtfProblems()
-
+  console.log(paramsId)
   return(
     <div className="inner-style">
       <MenuBar />
       <S.ProblemAreaStyled>
         <S.ProblemTopicStyled>Pwnable</S.ProblemTopicStyled>
         {problems.map(problem => {
-          const {title, score} = problem
+          const {id, title, score} = problem
           return(
-            <ProblemBox title={title} score={score} />
+            <>
+            <Link to={`/ctf/${id}`}><ProblemBox title={title} score={score} /></Link>
+            </>
           )
         })}
       </S.ProblemAreaStyled>
--- src/providers/CtfProviders.js
+++ src/providers/CtfProviders.js
@@ -4,32 +4,32 @@ import CtfContext from '../context/CtfContext'
 const CtfProviders = ({children}) => {
   const [problems, setProblems] = useState([
     {
-      id: "ctf-1",
+      id: 1,
       title: "버퍼 오버플로우 1",
       score: 10
     },
     {
-      id: "ctf-2",
+      id: 2,
       title: "버퍼 오버플로우 2",
       score: 20
     },
     {
-      id: "ctf-3",
+      id: 3,
       title: "버퍼 오버플로우 3",
       score: 30
     },
     {
-      id: "ctf-4",
+      id: 4,
       title: "버퍼 오버플로우 4",
       score: 40
     },
     {
-      id: "ctf-5",
+      id: 5,
       title: "버퍼 오버플로우 5",
       score: 50
     },
     {
-      id: "ctf-6",
+      id: 6,
       title: "버퍼 오버플로우 6",
       score: 60
     },
